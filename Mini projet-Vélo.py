617fefffffcdc6903522269919b6f861fe1eb4d1
Elie.Vullo <97040323+ElieVullo@users.noreply.github.com>
2022-01-25 11:55:28 +0100

    Update Mini projet-Vélo.py

--- Mini projet-Vélo.py
+++ Mini projet-Vélo.py
@@ -1,22 +1,20 @@
-import requests
+import requests 
 from lxml import etree
-response=requests.get("https://data.montpellier3m.fr/sites/default/files/ressources/TAM_MMM_VELOMAG.xml")
-print(response.text)
+from time import sleep
 
-velo=['TAM_MMM_VELOMAG']
-for i in velo:
-	response=requests.get("https://data.montpellier3m.fr/sites/default/files/ressources/" +i+ ".xml")
-	print(response.text)
+def makerequest():
+	response=response=requests.get("https://data.montpellier3m.fr/sites/default/files/ressources/TAM_MMM_VELOMAG.xml")
+	f1=open("velo.txt","w", encoding='utf8')
+	f1.write(response.text)
+	f1.close()
+	
+def parse():
+	tree = etree.parse("velo.txt")
+	for user in tree.xpath("/vcs/sl/si"):
+		print('Nom de la station: ' ,(user.get('na')))
+		print('Nombre de vélos libres : ' ,int(user.get('fr')))
 
-response=requests.get("https://data.montpellier3m.fr/sites/default/files/ressources/TAM_MMM_VELOMAG.xml")
-print(response.text)
-f1=open("TAM_MMM_VELOMAG.txt","w", encoding='utf8')
-f1.write(response.text)
-f1.close()
-tree = etree.parse("TAM_MMM_VELOMAG.txt")
-	for user in tree.xpath("na"):
-		print('Nom de la station de vélo',user.text)
-#for user in tree.xpath("av"):
-	#print('Nombre total de places :',user.text)
-	for user in tree.xpath("fr"):
-		print('Nombre de places libres :',user.text)
+for i in range(3):
+	makerequest()
+	parse()
+	time.sleep(10)
