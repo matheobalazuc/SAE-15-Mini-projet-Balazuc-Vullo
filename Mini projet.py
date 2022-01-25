ce4f5630f24d2a3551b4f79f7bcecd88d654a182
matheobalazuc <matheo.balazuc@etu.umontpellier.fr>
2022-01-25 10:41:17 +0100

    main

---
+++ Mini projet.py
@@ -0,0 +1,25 @@
+import requests 
+from lxml import etree 
+response=requests.get("https://data.montpellier3m.fr/sites/default/files/ressources/FR_MTP_GARE.xml") 
+print(response.text) 
+
+
+parkings=['FR_MTP_ANTI','FR_MTP_COME','FR_MTP_CORU','FR_MTP_EURO','FR_MTP_FOCH','FR_MTP_GAMB','FR_MTP_GARE','FR_MTP_TRIA','FR_MTP_ARCT',
+'FR_MTP_PITO','FR_MTP_CIRC','FR_MTP_SABI','FR_MTP_GARC','FR_MTP_MOSS','FR_STJ_SJLC','FR_MTP_MEDC','FR_MTP_OCCI','FR_CAS_VICA','FR_MTP_GA109','FR_MTP_GA250','FR_CAS_CDGA','FR_MTP_ARCE','FR_MTP_POLY']
+
+for i in parkings:
+    response=requests.get("https://data.montpellier3m.fr/sites/default/files/ressources/" +i+ ".xml") 
+    print(response.text)
+
+response=requests.get("https://data.montpellier3m.fr/sites/default/files/ressources/FR_MTP_COME.xml") 
+print(response.text) 
+f1=open("FR_MTP_COME.txt","w", encoding='utf8') 
+f1.write(response.text) 
+f1.close()
+tree = etree.parse("FR_MTP_COME.txt") 
+for user in tree.xpath("Name"): 
+    print('Nom du parking :',user.text) 
+for user in tree.xpath("Total"): 
+    print('Nombre total de places :',user.text) 
+for user in tree.xpath("Free"): 
+    print('Nombre de places libres :',user.
