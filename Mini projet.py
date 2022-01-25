a4a7988e9abe8ae586c68f782daa82a0e614aa96
matheobalazuc <matheo.balazuc@etu.umontpellier.fr>
2022-01-25 10:12:32 +0100

    main

--- Mini projet.py
+++ Mini projet.py
@@ -1,1 +1,7 @@
-azeazeaze
+import requests
+parkings=['FR_MTP_ANTI','FR_MTP_COME','FR_MTP_CORU','FR_MTP_EURO','FR_MTP_FOCH','FR_MTP_GAMB','FR_MTP_GARE','FR_MTP_TRIA','FR_MTP_ARCT','FR_MTP_PITO','FR_MTP_CIRC','FR_MTP_SABI','FR_MTP_GARC','FR_MTP_SABL','FR_MTP_MOSS','FR_STJ_SJLC','FR_MTP_MEDC','FR_MTP_OCCI','FR_CAS_VICA','FR_MTP_GA109','FR_MTP_GA250','FR_CAS_CDGA','FR_MTP_ARCE','FR_MTP_POLY'] 
+
+for i in range(len(parkings)):
+	url="https://data.montpellier3m.fr/sites/default/files/ressources/"+parkings[i]+".xml"
+	response=requests.get(url)
+	print(response.text)
