308666a4898e894d6d05ed75267f404d774c3b3c
matheobalazuc <matheo.balazuc@etu.umontpellier.fr>
2022-01-30 10:10:59 +0100

    main

--- Mini projet Voiture/Mini projet-Voiture.py
+++ Mini projet Voiture/Mini projet-Voiture.py
@@ -1,5 +1,9 @@
+from encodings import utf_8
 import requests #requests est un module python permettant d'utiliser le protocole http
 from lxml import etree #pour importer le librairie html
+from lxml import etree
+import time
+import PyGnuplot
 
 parkings=['FR_MTP_ANTI','FR_MTP_COME','FR_MTP_CORU','FR_MTP_EURO','FR_MTP_FOCH','FR_MTP_GAMB','FR_MTP_GARE','FR_MTP_TRIA','FR_MTP_ARCT',
 'FR_MTP_PITO','FR_MTP_CIRC','FR_MTP_SABI','FR_MTP_GARC','FR_MTP_MOSS','FR_STJ_SJLC','FR_MTP_MEDC','FR_MTP_OCCI','FR_CAS_VICA','FR_MTP_GA109','FR_MTP_GA250','FR_CAS_CDGA','FR_MTP_ARCE','FR_MTP_POLY'] #voici la liste des différents parkings
