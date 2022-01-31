b4975a9dfb703c36d14d9a9f5201a4b514065835
matheobalazuc <matheo.balazuc@etu.umontpellier.fr>
2022-01-31 19:25:22 +0100

    main

--- Mini projet Voiture/Mini projet-Voiture.py
+++ Mini projet Voiture/Mini projet-Voiture.py
@@ -8,12 +8,16 @@ import time
 parkings=['FR_MTP_ANTI','FR_MTP_COME','FR_MTP_CORU','FR_MTP_EURO','FR_MTP_FOCH','FR_MTP_GAMB','FR_MTP_GARE','FR_MTP_TRIA','FR_MTP_ARCT',
 'FR_MTP_PITO','FR_MTP_CIRC','FR_MTP_SABI','FR_MTP_GARC','FR_MTP_MOSS','FR_STJ_SJLC','FR_MTP_MEDC','FR_MTP_OCCI','FR_CAS_VICA','FR_MTP_GA109','FR_MTP_GA250','FR_CAS_CDGA','FR_MTP_ARCE','FR_MTP_POLY'] #voici la liste des différents parkings
 
+
+
 for i in parkings: #pour un nombre quelconque (i) dans la liste des parkings 
 	response=requests.get("https://data.montpellier3m.fr/sites/default/files/ressources/" +i+ ".xml") #resquest.get est utilisée pour demander une ressource au serveur
 	f1=open("Voiture.txt","w", encoding='utf8') #ouverture du fichier avec un encoding utf8 qui est un codage de caractères informatiques permettant de coder l'ensemble des caractères internationaux
 	f1.write(response.text) #écriture du fichier en text
 	f1.close()
 	tree = etree.parse("Voiture.txt") #fonction pour analyser et créer des données XML.
+	
+	
 	f1=open("Resultat.txt","a", encoding='utf8')
 	for user in tree.xpath("Name"): #le programme récupère les données dans la variable "Name"(nom du parking) du fichier .txt
 		print('Nom du parking :',user.text)
@@ -30,4 +34,9 @@ for i in parkings: #pour un nombre quelconque (i) dans la liste des parkings
 		f1.write("Nombres de places au total :")
 		f1.write("\n")
 		f1.write(user.text)
+print(Données)
+print("Attend 10 secondes")
+time.sleep(10)
+
+
 
