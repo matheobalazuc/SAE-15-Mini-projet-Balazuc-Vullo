77ac718d4cebc7c80b3704443a448a9f073c9768
matheobalazuc <matheo.balazuc@etu.umontpellier.fr>
2022-02-01 23:21:30 +0100

    main

--- Mini projet Vélo/Mini projet-Vélo.py
+++ Mini projet Vélo/Mini projet-Vélo.py
@@ -3,31 +3,28 @@ import time                                        #Fournit un tas de fonction l
 from lxml import etree                             #Fournit la librairie html
 from time import sleep                             #On import la fonction "sleep" depuis "temps" 
 
-bike=20
-		
-for i in range(bike):                                 #Définition de la fonction qui va ouvrir le fichier des données sur le vélo.
-	response=requests.get("https://data.montpellier3m.fr/sites/default/files/ressources/TAM_MMM_VELOMAG.xml")     #Sert à demander une ressource au serveur.
-	f1=open("vélo.txt","w", encoding='utf8')       #Ouvre le fichier velo.txt en écriture en utf8
-	f1.write(response.text)                        #On écrit dans le fichier en text
-	f1.close()                                     #On ferme le fichier
+                                 
+response=requests.get("https://data.montpellier3m.fr/sites/default/files/ressources/TAM_MMM_VELOMAG.xml")     #Sert à demander une ressource au serveur.
+f1=open("vélo.txt","w", encoding='utf8')       #Ouvre le fichier velo.txt en écriture en utf8
+f1.write(response.text)                        #On écrit dans le fichier en text
+f1.close()                                     #On ferme le fichier
 	
-	f1=open("vélo_résultat.txt","a", encoding='utf8')                 #Créer la variable tree qui est égal à l'analyse et création de données du fichier contenant toutes les informations sur les vélos dans le fichier velo.txt
-	tree = etree.parse("vélo.txt")
-	for user in tree.xpath("/vcs/sl/si"):         #Boucle qui va récupérer les données dans le fichier de données des vélos dans les balises html vcs/sl/si .   #Ouvre un fichier texte nommé velo.txt en écriture en utf8
-		f1.write(f"Nom de la station : ")                       #Ecrit dans le fichier le temps 
-		f1.write(user.get("na"))                   #Ecrit dans le fichier les données de "fr", donc les places disponibles.
+f1=open("vélo_résultat.txt","a", encoding='utf8')                 
+tree = etree.parse("vélo.txt")
+for user in tree.xpath("/vcs/sl/si"):         #Boucle qui va récupérer les données dans le fichier de données des vélos dans les balises html vcs/sl/si .  
+		f1.write(f"Nom de la station : ")       #Nom de la station a afficher dans le vélo_résultat.txt a la suite de la donnée récupéré            
+		f1.write(user.get("na"))                   #Récupère les données du serveur sur le nom de la station    
+		f1.write("\n")								#Saut de ligne pour mieux organiser le fichier du résultat : vélo_résultat.txt
+		f1.write(f"Places libres : ")               #Places libres a afficher dans le vélo_résultat.txt a la suite de la donnée récupéré       
+		f1.write(user.get("fr"))                   #Récupère les données du serveur sur le nombre de places libres
 		f1.write("\n")
-		f1.write(f"Places libres : ")                       #Ecrit dans le fichier le temps 
-		f1.write(user.get("fr"))                   #Ecrit dans le fichier les données de "fr", donc les places disponibles.
+		f1.write(f"Places occupées : ")            #Places occupées a afficher dans le vélo_résultat.txt a la suite de la donnée récupéré          
+		f1.write(user.get("av"))                  #Récupère les données du serveur sur le nombre de places occuppées 
 		f1.write("\n")
-		f1.write(f"Places occupées : ")                       #Ecrit dans le fichier le temps 
-		f1.write(user.get("av"))                   #Ecrit dans le fichier les données de "fr", donc les places disponibles.
-		f1.write("\n")
-		f1.write(f"Nombre de places totales : ")                       #Ecrit dans le fichier le temps 
-		f1.write(user.get("fr"))                   #Ecrit dans le fichier les données de "fr", donc les places disponibles.
+		f1.write(f"Nombre de places totales : ")     #Nombre de places totales a afficher dans le vélo_résultat.txt a la suite de la donnée récupéré                  
+		f1.write(user.get("fr"))                   #Récupère les données du serveur sur le nombre de place totales
 		f1.write("\n") 
-		f1.write("\n")                            #Permet d'aller à la ligne dans l'écriture du fichier
+		f1.write("\n")                           #Saut de ligne pour mieux organiser le fichier du résultat : vélo_résultat.txt
 		  
-		                       #On ferme le fichier
-                                  #On utilise la fonction parse
+		                       
 	
