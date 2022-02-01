1a35fbaf85496e7d24fdf12237947be8c14f0b8b
matheobalazuc <matheo.balazuc@etu.umontpellier.fr>
2022-02-02 00:29:44 +0100

    main

--- Mini projet Voiture/Mini projet-Voiture.py
+++ Mini projet Voiture/Mini projet-Voiture.py
@@ -41,6 +41,10 @@ for i in parkings: #pour un nombre quelconque (i) dans la liste des parkings
 		f1.write(user.text)
 		f1.write("\n")
 		f1.write("\n")
+		f1.write(f"------- Actualisation des parkings dans 30 minutes -------")
+		f1.write("\n")
+		f1.write("\n")
+time.sleep(10)   
 
 
 
